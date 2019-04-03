f908ab84629a797b346952e8e2cb544695cf8381
Karl Kuhn <karljkuhn@gmail.com>
2019-04-02 23:54:05 -0400

    add some more sorts

--- folderBuilder/folderBuilder.js
+++ folderBuilder/folderBuilder.js
@@ -16,16 +16,18 @@ function FolderBuilder(){
 		}
 		this.buildTable();
 	}
-	sortDirs = {'name':0,'damage':0,'hits':0,'copies':0,'goo':0};
+	sortDirs = {'name':0,'code':0,'damage':0,'hits':0,'copies':0,'rank':0,'goo':0};
 	
 	this.buildTable = function(){
 		var table = document.getElementById("myTable");
 		table.innerHTML = `<tr> 
 				    		<th onclick="folderBuilder.sortFolderBuilder('name',${!sortDirs.name})">Image</th> 
+							<th onclick="folderBuilder.sortFolderBuilder('code',${!sortDirs.code})">Code</th>
 				    		<th onclick="folderBuilder.sortFolderBuilder('name',${!sortDirs.name})">Name</th> 
 				    		<th onclick="folderBuilder.sortFolderBuilder('damage',${!sortDirs.damage})">Damage</th>
 				    		<th onclick="folderBuilder.sortFolderBuilder('hits',${!sortDirs.hits})">Hits</th>
 				    		<th onclick="folderBuilder.sortFolderBuilder('copies',${!sortDirs.copies})">Copies</th>
+							<th onclick="folderBuilder.sortFolderBuilder('rank',${!sortDirs.rank})">Rank</th>
 				    		<th onclick="folderBuilder.sortFolderBuilder('goo',${!sortDirs.goo})">GOO</th>
 				  		 </tr>`;
 
@@ -37,12 +39,23 @@ function FolderBuilder(){
 	  		var cell3 = row.insertCell(3);
 	  		var cell4 = row.insertCell(4);
 	  		var cell5 = row.insertCell(5);
-	  		cell0.innerHTML = "Image Here";
-	  		cell1.innerHTML = BUILDABLECARDS[i].name;
-	  		cell2.innerHTML = BUILDABLECARDS[i].damage;
-	  		cell3.innerHTML = BUILDABLECARDS[i].hits;
-	  		cell4.innerHTML = BUILDABLECARDS[i].copies;
-	  		cell5.innerHTML = BUILDABLECARDS[i].goo;
+			var cell6 = row.insertCell(6);
+			var cell7 = row.insertCell(7);
+			var img = document.createElement('img');
+	  		cell0.innerHTML = "";
+			cell0.appendChild(BUILDABLECARDS[i].image);
+			if(BUILDABLECARDS[i].code.length === 26){
+				cell1.innerHTML = "*";
+			}
+			else{
+				cell1.innerHTML = BUILDABLECARDS[i].code;
+			}
+	  		cell2.innerHTML = BUILDABLECARDS[i].name;
+	  		cell3.innerHTML = BUILDABLECARDS[i].damage;
+	  		cell4.innerHTML = BUILDABLECARDS[i].hits;
+	  		cell5.innerHTML = BUILDABLECARDS[i].copies;
+			cell6.innerHTML = BUILDABLECARDS[i].rank;
+	  		cell7.innerHTML = BUILDABLECARDS[i].goo;
 		}
 	};
 }
